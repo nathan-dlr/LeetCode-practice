b65b3d512a6acb58edf278d9f617bb9555e76261
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2024-06-06 12:13:38 -0500

    Time: 0 ms (100.00%) | Memory: 9.8 MB (24.58%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int current_alt = 0;
+        int max_alt = 0;
+
+        for (int i = 0; i < gain.size(); i++) {
+            current_alt += gain[i];
+
+            if (current_alt > max_alt) {
+                max_alt = current_alt;
+            }
+        }
+        return max_alt;
+    }
+};
