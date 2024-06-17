f72f2715072fb8753e81d664bb40734516f8452d
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2024-06-17 18:36:56 -0500

    Time: 11 ms (46.57%) | Memory: 20 MB (94.31%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> result;
         for (int i = 0; i < asteroids.size(); i++) {
-            cout << asteroids[i] << endl;
             if (result.empty()) {
                 result.push_back(asteroids[i]);
             }
@@ -16,13 +15,11 @@ public:
                     while (cond)
                         if (result.empty()) {
                             result.push_back(asteroids[i]);
-                            cout << "pushing bc empty" << endl;
                             cond = false;
                         }
                         else if (result.back() < 0) {
                             result.push_back(asteroids[i]);
                             cond = false; 
-                            cout << "pushing bc negative" << endl;
                         }
                         else if (abs(result.back()) < abs(asteroids[i])) {
                             result.pop_back();
