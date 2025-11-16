2a6ce8871c1dc01d807d21aa0f01c20ac5fcf684
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2025-11-16 02:37:37 -0600

    Time: 0 ms (100.00%) | Memory: 11.5 MB (72.24%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int numSub(string s) {
-        bool isSubStr = false;
         int onesInSubStr = 0;
         int total = 0;
+        int mod = 1000000007;
         for (char c : s) {
-            if (c == '0' && !isSubStr) {
-                continue;
-            }
-            else if (c == '0' && isSubStr) {
+            if (c == '0') {
                 onesInSubStr = 0;
-                isSubStr = false;
             }
-            else if (c == '1' && isSubStr) {
+            if (c == '1') {
                 onesInSubStr++;
-                total = (total + onesInSubStr) % 1000000007;
-            }
-            else if (c == '1' && !isSubStr) {
-                total += ++onesInSubStr;
-                isSubStr = true;
+                total += onesInSubStr;
+                if (total > mod) {
+                    total %= mod;
+                }
             }
         }
         return total;
