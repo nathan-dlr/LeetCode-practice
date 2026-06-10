be91acb96e774efc34666a5c3542d02ce4f640b9
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-06-10 15:02:16 -0500

    Time: 7 ms (5.31%) | Memory: 42.3 MB (16.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> output;
-        vector<int> forward;
-        vector<int> backward;
-        forward.push_back(nums[0]);
-        backward.push_back(nums.back());
-        int length = nums.size() - 1;
+        vector<int> prefix;
+        vector<int> suffix(nums.size());
+        vector<int> result(nums.size());
+        prefix.reserve(nums.size());
+        prefix.push_back(1);
+        suffix[nums.size() - 1] = 1;
 
-        for (int i = 1; i < nums.size(); i++) {
-            forward.push_back(nums[i] * forward[i-1]);
+        for (int i = 1, j = nums.size()-2; i < nums.size(); i++, j--) {
+            prefix.push_back(prefix[i-1] * nums[i - 1]);
+            suffix[j] = suffix[j+1] * nums[j + 1];
         }
 
-        for (int j = 1; j < nums.size(); j++) {
-            backward.push_back(nums[length - j] * backward[j-1]);
+        result[0] = suffix[0];
+        result[nums.size() - 1] = prefix[nums.size() - 1];
+        for (int i = 1; i < nums.size() - 1; i++) {
+            result[i] = prefix[i] * suffix[i];
         }
-
-        int i = 0;
-        for (int k = 1; k <= nums.size(); k++) {
-            if (k == 1) {
-                output.push_back(backward[length - k]);
-            }
-            else if (k < nums.size()) {
-                output.push_back(backward[length - k] * forward[i]);
-                i++;
-            }
-            else {
-                output.push_back(forward[i]);
-            }
-        }
-        
-        return output;
+        return result;
     }
 };
