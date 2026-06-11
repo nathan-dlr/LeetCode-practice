6fd4d2a09d6c006975d2686fcc2fb0bce9e6207f
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-06-11 16:12:47 -0500

    Time: 2 ms (39.11%) | Memory: 62.9 MB (77.83%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,22 +3,18 @@ public:
     int maxArea(vector<int>& height) {
         int x1 = 0;
         int x2 = height.size() - 1;
-        int max = (height[x1] > height[x2]) ? ((x2-x1) * (height[x2])) : (((x2-x1) * (height[x1])));
-        int current; 
+        int max = 0;
 
         for (int i = 0; i < height.size(); i++) {
+            int curr = std::min(height[x1], height[x2]) * (x2-x1);
+            max = std::max(curr, max);
+        
             if (height[x1] > height[x2]) {
                 x2--;
             }
             else {
                 x1++;
             }
-            if (x1 == x2) {
-                break;
-            }
-
-            current = (height[x1] > height[x2]) ? ((x2-x1) * (height[x2])) : (((x2-x1) * (height[x1])));
-            max = (max >= current) ? max : current;
         }
 
 
