9fe4c3860cb5ce9c1e89fa36ddd78af8fab36c2d
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-06-18 19:42:54 -0500

    Time: 0 ms (100.00%) | Memory: 9 MB (18.86%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if (s.size() % 2) {
-            return false;
-        }
         // Use a stack to keep track of the past braces
         std::stack<char> braces;
-        std::unordered_map<char, char> map;
-        map['('] = ')';
-        map['{'] = '}';
-        map['['] = ']';
+    
 
         for (char c : s) {
             // iterate through the string
@@ -17,14 +11,23 @@ public:
                 braces.push(c);
             }
             // If we see a closing brace, pop from the stack to find matching 
-            else if (!braces.empty() && map.at(braces.top()) == c) {
-                braces.pop();
+            else if (braces.empty()) {
+                return false;
             }
             // If it doesnt match, return false
             else {
-                return false;
+                char top = braces.top();
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+                else if (c == '}' && top != '{') {
+                    return false;
+                }
+                else if (c == ']' && top != '[') {
+                    return false;
+                }
+                braces.pop();
             }
-
         }
         return braces.empty();
     }
