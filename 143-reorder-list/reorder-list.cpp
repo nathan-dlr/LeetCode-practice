4d6c0bd353d5e917de643fb57ce3430c2963b548
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-06-23 16:32:26 -0500

    Time: 4 ms (9.11%) | Memory: 23 MB (28.59%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,36 +11,36 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        // need access to node 0 and node n
-        // niave aproach, traverse LL, for every other node insert the tail - O(N^2)
-        // O(N) approach, store each node in a vector, that way we have easier access to the back of the list
-        // Fill vector of nodes
-        std::vector<ListNode*> nodes;
-        while (head) {
-            nodes.push_back(head);
-            head = head->next;
-        }
-        
-        if (nodes.size() <= 2) {
-            return;
-        }
-        // traverse the vector, removing the last and inserting it between nodes
-        int i;
-        for (i = 0; i < nodes.size() - 2; i++) {
-            ListNode* insert = nodes.back();
-            nodes.pop_back();
-            insert->next = nodes[i]->next;
-            nodes[i]->next = insert;
+        // find middle using slow/fast method
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        // Close the linked list with null ptr
-        if (i == nodes.size() - 2) {
-            nodes[i+1]->next = nullptr;
-            return;
+        // reverse second half
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+        ListNode* next;
+        while (curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        else if (i == nodes.size() - 1) {
-            nodes[i]->next = nullptr;
-            return;
+        // interleave nodes
+        ListNode* reorder = prev;
+        while (reorder) {
+            next = reorder->next;
+
+            reorder->next = head->next;
+            head->next = reorder;
+
+            head = reorder->next;
+            reorder = next;
         }
+        head->next = nullptr;
     }
 };
