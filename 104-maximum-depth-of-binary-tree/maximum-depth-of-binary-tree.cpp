9ef06ffbd3fdb8f5c7adda2549171b4290fc51bb
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-06-26 14:52:04 -0500

    Time: 0 ms (100.00%) | Memory: 19 MB (47.70%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,25 +12,9 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (root == nullptr) {return 0;}
-        int right_depth = 0;
-        int left_depth = 0;
-        if (root->left == nullptr && root->right == nullptr) {
-            return 1;
+        if (!root) {
+            return 0;
         }
-        if (root->left) {
-            right_depth = 1 + maxDepth(root->left);
-        }
-        
-        if (root->right) {
-            left_depth = 1 + maxDepth(root->right);
-        }
-        if (right_depth > left_depth) {
-            return right_depth;
-        }
-        else {
-            return left_depth;
-        }
-
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
