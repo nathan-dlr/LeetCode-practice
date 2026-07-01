197b49fdbf83981d88fc3907aa8e586923943148
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-07-01 17:52:55 -0500

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.69%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // Sort in decending order to easiler iterate from largest to smallest 
-        std::sort(candidates.begin(), candidates.end(), std::greater<>());
-        // All possible sums that may add up to target
-        std::queue<vector<int>> combinations;
-        std::vector<vector<int>> sums;
-        
-        for (int i = 0; i < candidates.size(); i++) {
-            if (candidates[i] == target) {
-                sums.push_back({candidates[i]});
-                continue;
-            }
-            else if (candidates[i] < target) {
-                combinations.push({candidates[i]});
-            }
-            else {
-                continue;
-            }
-            // start with our current possible subsets and try to build a sum to target with each of them
-            // avoid iterating the same subsets by keeping the current candidate's possible subsets seperate from the list of all possible subsets
-            std::queue<vector<int>> curSubsets = combinations;
-            while (!curSubsets.empty()) {
-                int size = curSubsets.size();
-                for (int j = 0; j < size; j++) {
-                    std::vector<int> subset = curSubsets.front();
-                    curSubsets.pop();
-        
-                    int sum = std::accumulate(subset.begin(), subset.end(), candidates[i]);
-                    if (sum == target) {
-                        subset.push_back(candidates[i]);
-                        sums.emplace_back(subset);
-                    }
-                    // this subset is possible for our current candidate and future candidates so add to both queues
-                    else if (sum < target) {
-                        subset.push_back(candidates[i]);
-                        curSubsets.push(subset);
-                        combinations.push(subset);
-                    }
-                }
-            }
+    void backtrack(vector<int>& candidates, int remaining, int start, vector<vector<int>>& sums, vector<int>& path) {
+        if (remaining == 0) {
+            sums.push_back(path);
+            return;
         }
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > remaining) break;
+            path.push_back(candidates[i]);
+            backtrack(candidates, remaining - candidates[i], i, sums, path);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        std::sort(candidates.begin(), candidates.end());
+        std::vector<vector<int>> sums;
+        std::vector<int> path;
+
+        backtrack(candidates, target, 0, sums, path);
         return sums;
     }
 };
