873a74535c103dfc9c0a9acb37c164280783fffc
nathan-dlr <154288475+nathan-dlr@users.noreply.github.com>
2026-07-12 15:38:33 -0500

    Time: 0 ms (100.00%) | Memory: 24.1 MB (44.87%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if (n == 0) {
+        if (!n || (flowerbed.size() == 1 && flowerbed[0] == 0 && n == 1)) {
             return true;
         }
-        for (int i = 0; i < flowerbed.size(); i++) {
-            int flower = flowerbed.at(i);
-            if (flower) {
-                i++;
-            }
-            else {
-                if (i == flowerbed.size() - 1) {
-                    n--;
-                    i++;
-                }
-                else if (!flowerbed[i+1]) {
-                    n--;
-                    i++;
-                }
-            }
-            if (!n) {
-                return true;
+        if (flowerbed[0] == 0 && flowerbed.size() != 1 && flowerbed[1] == 0) {
+            flowerbed[0] = 1;
+            n--;
+        }
+        for (int i = 1; i < flowerbed.size() && n > 0; i++) {
+            bool isZero = flowerbed[i] == 0;
+            bool leftZero = flowerbed[i-1] == 0;
+            bool rightZero = (flowerbed.size() != i + 1) && (flowerbed[i+1] == 0);
+            if (isZero && leftZero && rightZero) {
+                flowerbed[i] = 1;
+                n--;
             }
         }
-        return false;
+        bool isZero = flowerbed[flowerbed.size() - 1] == 0;
+        bool leftZero = (flowerbed.size() > 1) && (flowerbed[flowerbed.size() - 2] == 0);
+        if (isZero && leftZero) {
+            flowerbed[flowerbed.size() - 1] = 1;
+            n--;
+        }
+        return n <= 0;
     }
 };
